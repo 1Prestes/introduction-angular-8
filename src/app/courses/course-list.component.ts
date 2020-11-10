d8eedbed109928eb9d9f7702638bab668b24c4e9
Dijango Alves <dijango_ilha@hotmail.com>
2020-11-10 16:00:35 -0300

    refactor: move template to external template

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -1,10 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-course-list',
-    template: '<h2>Course List</h2>'
+  selector: 'app-course-list',
+  templateUrl: './course-list.component.html',
 })
-
-export class CourseListComponent {
-
-}
+export class CourseListComponent {}
