c4e78729589c32e4bb0cccf1b8830b967204aa24
Dijango Alves <dijango_ilha@hotmail.com>
2020-11-10 16:53:11 -0300

    feat: add courses

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -1,7 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Course } from './curse';
 
 @Component({
   selector: 'app-course-list',
   templateUrl: './course-list.component.html',
 })
-export class CourseListComponent {}
+export class CourseListComponent implements OnInit {
+  courses: Course[] = [];
+
+  ngOnInit(): void {
+    this.courses = [
+      {
+        id: 1,
+        name: 'Angular: Forms',
+        imageUrl: '',
+        price: 500.2,
+        code: 'XPS-8796',
+        duration: 120,
+        rating: 5,
+        releaseDate: 'November, 2, 2019',
+      },
+      {
+        id: 1,
+        name: 'Angular: HTTP',
+        imageUrl: '',
+        price: 400.2,
+        code: 'LKL-1020',
+        duration: 80,
+        rating: 5,
+        releaseDate: 'November, 4, 2019',
+      },
+    ];
+  }
+}
