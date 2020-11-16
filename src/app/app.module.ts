20a5a410fa9ba9764ce4d56376b63d800c820f68
Dijango Alves <dijango_ilha@hotmail.com>
2020-11-16 19:52:02 -0300

    refactor: segregate page 404 to a generic component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,9 @@ import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { Error404Component } from './error-404/error-404.component';
 
 @NgModule({
-  declarations: [AppComponent, Error404Component],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     HttpClientModule,
@@ -20,10 +19,6 @@ import { Error404Component } from './error-404/error-404.component';
         path: '',
         redirectTo: 'courses',
         pathMatch: 'full',
-      },
-      {
-        path: '**',
-        component: Error404Component,
       },
     ]),
   ],
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,18 @@
+import { Error404Component } from './components/error-404/error-404.component';
 import { RouterModule } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { NgModule } from '@angular/core';
 
 @NgModule({
-  declarations: [NavbarComponent],
-  imports: [RouterModule],
+  declarations: [NavbarComponent, Error404Component],
+  imports: [
+    RouterModule.forChild([
+      {
+        path: '**',
+        component: Error404Component,
+      },
+    ]),
+  ],
   exports: [NavbarComponent],
 })
 export class CoreModule {}
